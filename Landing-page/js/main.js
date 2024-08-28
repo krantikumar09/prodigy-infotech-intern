4cf2da83b52be785051300a65172e4fdaf85025f
Krantikumar Mathapati <krantikumar3446@gmail.com>
2024-08-28 12:09:02 +0530

    complete testimonial
    completed testimonial section with responsiveness.

--- Landing-page/js/main.js
+++ Landing-page/js/main.js
@@ -1,15 +1,19 @@
+// swiper
+// import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
 // navbar
 const navbar = document.getElementById("navbar");
 
-window.addEventListener('scroll', () => {
-    navbar.classList.toggle('navbar-shadow', window.scrollY > 0)
-})
-
+window.addEventListener("scroll", () => {
+  navbar.classList.toggle("navbar-shadow", window.scrollY > 0);
+});
 
 // mobile nav
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("active")
-})
+  menu.classList.toggle("active");
+});
+
+
